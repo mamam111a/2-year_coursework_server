70674ad0728f798d597dccebce0d796b5a95746f
owner <owner@mail.ru>
2025-11-10 02:44:53 +0700

    add user menu

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-1. Инструкция по запуску сервера на Ubuntu/Debian дистрибутиве:
+Инструкция по запуску сервера на Ubuntu/Linux:
 
-1.1 Установка Docker (если ещё не установлен)
+0) Установка Docker (если ещё не установлен)
 
 Выполните в терминале команду:
 "sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin && docker --version"
 
-1.2 Подготовка и запуск сервера
+1) Подготовка и запуск сервера
 
-Перейдите в главную директорию проекта, где находится текущий файл README.md.
+Перейдите в главную директорию, где находится файл README.md.
 Откройте терминал в этой директории.
 Загрузите Docker-образ и запустите сервер, выполнив команду:
 "docker load -i ./images/ubuntu-24.04.tar && docker compose up --build -d"
 
-1.3 Просмотр логов сервера
+2) Просмотр логов сервера
 "docker logs coursework-server_container"
-
--- server/DBMSbody.cpp
+++ server/DBMSbody.cpp
@@ -28,48 +28,88 @@ bool DBMS_Queries(int& clientSocket, string& command, ostringstream& toClient, s
     while (getline(ss, word, '|')) {
         elements.push_back(word);
     }
-    string priceOt, priceDo, quantityOt, quantityDo ;
-    if(role == "user") {
-        priceOt = elements[8];
-        priceDo= elements[9];
-        quantityOt = elements[6];
-        quantityDo = elements[7];
-        elements.pop_back();
-        elements.pop_back();
-        elements.pop_back();
-        elements.pop_back();
-    }
+
     ConceptTable tableBooks("books/books.json", username);
     ConceptTable tableShops("shops/shops.json", username);
     if(role == "user" ) {
-        int intListShops = elements.size() - 1;
-        string strListShops = elements[intListShops];
-        elements.pop_back();
+        if(elements[0] == "findshops") {
+            string query;
+            elements.erase(elements.begin());
+            for(int i = 0; i < elements.size(); i++) {
+                if(elements[i].empty()) continue;
+                string category;
+                if(i==0) category = "name";
+                else if(i == 1) category = "address";
+                else if(i == 2) category = "working_hours";
+                query += "shops." + category + " = '" + elements[i] + "' AND ";
+            }
+            query.erase(query.size() - 5);
+            if (FindByCriteria(query,username)) {
+                ifstream finalFile("finalFile_" + username + ".tmp");
+                ostringstream oss;
+                oss << finalFile.rdbuf(); 
+                finalFile.close();
+                string message = oss.str();
+                toClient << "%";
+                toClient << message;    
+    
+                return true;
+            } else {
+                toClient << "!Ничего не найдено :( ))";
+                return false;
+            }
+        }
+        else{
+            string priceOt, priceDo, quantityOt, quantityDo ;
+
+            priceOt = elements[8];
+            priceDo= elements[9];
+            quantityOt = elements[6];
+            quantityDo = elements[7];
+            elements.pop_back();
+            elements.pop_back();
+            elements.pop_back();
+            elements.pop_back();
+            
+            int intListShops = elements.size() - 1;
+            string strListShops = elements[intListShops];
+            elements.pop_back();
 
 
-        vector<string> andElements;        
-        vector<vector<string>> orElements;  
-        for (int i = 0; i < elements.size(); i++) {
-            if (elements[i].empty()) continue;
+            vector<string> andElements;        
+            vector<vector<string>> orElements;  
+            for (int i = 0; i < elements.size(); i++) {
+                if (elements[i].empty()) continue;
 
-            if (elements[i].find(u8"или") != string::npos) {
-                string tmp = elements[i];
-                int pos = 0;
-                while ((pos = tmp.find(u8"или", pos)) != string::npos) {
-                    tmp.replace(pos, string(u8"или").size(), "|");
-                    pos += 1;
-                }
+                if (elements[i].find(u8"или") != string::npos) {
+                    string tmp = elements[i];
+                    int pos = 0;
+                    while ((pos = tmp.find(u8"или", pos)) != string::npos) {
+                        tmp.replace(pos, string(u8"или").size(), "|");
+                        pos += 1;
+                    }
 
-                stringstream ssTmp(tmp);
-                string wordOr;
-                vector<string> orConds;
+                    stringstream ssTmp(tmp);
+                    string wordOr;
+                    vector<string> orConds;
 
-                while (getline(ssTmp, wordOr, '|')) {
-                    int start = wordOr.find_first_not_of(" \t");
-                    int end = wordOr.find_last_not_of(" \t");
-                    if (start != string::npos && end != string::npos)
-                        wordOr = wordOr.substr(start, end - start + 1);
+                    while (getline(ssTmp, wordOr, '|')) {
+                        int start = wordOr.find_first_not_of(" \t");
+                        int end = wordOr.find_last_not_of(" \t");
+                        if (start != string::npos && end != string::npos)
+                            wordOr = wordOr.substr(start, end - start + 1);
 
+                        string category;
+                        if(i == 0) category = "section";
+                        else if(i == 1) category = "author";
+                        else if(i == 2) category = "title";
+                        else if(i == 3) category = "publisher";
+                        else if(i == 4) category = "publishing_year";
+
+                        orConds.push_back("books." + category + " = '" + wordOr + "'");
+                    }
+                    orElements.push_back(orConds);
+                } else {
                     string category;
                     if(i == 0) category = "section";
                     else if(i == 1) category = "author";
@@ -77,95 +117,85 @@ bool DBMS_Queries(int& clientSocket, string& command, ostringstream& toClient, s
                     else if(i == 3) category = "publisher";
                     else if(i == 4) category = "publishing_year";
 
-                    orConds.push_back("books." + category + " = '" + wordOr + "'");
-                }
-                orElements.push_back(orConds);
-            } else {
-                string category;
-                if(i == 0) category = "section";
-                else if(i == 1) category = "author";
-                else if(i == 2) category = "title";
-                else if(i == 3) category = "publisher";
-                else if(i == 4) category = "publishing_year";
-
-                andElements.push_back("books." + category + " = '" + elements[i] + "'");
-            }
-        }
-
-        if (!strListShops.empty() && stoi(strListShops) != 0) {
-            stringstream sss(strListShops);
-            string number;
-            vector<string> shopConds;
-            while (getline(sss, number, ',')) {
-                shopConds.push_back("books.shop_id = '" + number + "'");
-            }
-            orElements.insert(orElements.begin(), shopConds); 
-        }
-        vector<string> combined;
-        combined.push_back("");
-
-        for (auto& orBlock : orElements) {
-            vector<string> newCombined;
-            for (auto& prefix : combined) {
-                for (auto& cond : orBlock) {
-                    string tmp = prefix;
-                    if (!tmp.empty()) tmp += " AND ";
-                    tmp += cond;
-                    newCombined.push_back(tmp);
+                    andElements.push_back("books." + category + " = '" + elements[i] + "'");
                 }
             }
-            combined = newCombined;
-        }
 
-        for (auto& andEl : andElements) {
-            for (auto& s : combined) {
-                if (!s.empty()) s = andEl + " AND " + s;
-                else s = andEl;
+            if (!strListShops.empty() && stoi(strListShops) != 0) {
+                stringstream sss(strListShops);
+                string number;
+                vector<string> shopConds;
+                while (getline(sss, number, ',')) {
+                    shopConds.push_back("books.shop_id = '" + number + "'");
+                }
+                orElements.insert(orElements.begin(), shopConds); 
             }
-        }
-        string query = "";
-        for (auto& s : combined) query += s + " OR ";
-        query.erase(query.size() - 4); 
-        toClient.str("");
-        toClient.clear();
-        cout << endl << query << endl;
-        if (FindByCriteria(query, username)) {
-            ifstream finalFile("finalFile_" + username + ".tmp");
-            ofstream finalFinalFile("finalFileAfterChecking_" + username + ".tmp");
-            string temp;
-            bool rowAdded = false;
-        
-            while(getline(finalFile, temp)) {
-                string quantity = GetCellByIndex(temp, 7);
-                string price = GetCellByIndex(temp, 8);
-                if((stoi(quantity) >= stoi(quantityOt) && stoi(quantity) <= stoi(quantityDo)) && 
-                ( stoi(price)  >= stoi(priceOt) && stoi(price) <= stoi(priceDo))) { 
-                    finalFinalFile << temp << endl; rowAdded = true; 
+            vector<string> combined;
+            combined.push_back("");
+
+            for (auto& orBlock : orElements) {
+                vector<string> newCombined;
+                for (auto& prefix : combined) {
+                    for (auto& cond : orBlock) {
+                        string tmp = prefix;
+                        if (!tmp.empty()) tmp += " AND ";
+                        tmp += cond;
+                        newCombined.push_back(tmp);
+                    }
+                }
+                combined = newCombined;
+            }
+
+            for (auto& andEl : andElements) {
+                for (auto& s : combined) {
+                    if (!s.empty()) s = andEl + " AND " + s;
+                    else s = andEl;
                 }
             }
-        
-            finalFinalFile.close();
-            finalFile.close();
-        
-            if(!rowAdded) {
+            string query = "";
+            for (auto& s : combined) query += s + " OR ";
+            query.erase(query.size() - 4); 
+            toClient.str("");
+            toClient.clear();
+            cout << endl << query << endl;
+            if (FindByCriteria(query, username)) {
+                ifstream finalFile("finalFile_" + username + ".tmp");
+                ofstream finalFinalFile("finalFileAfterChecking_" + username + ".tmp");
+                string temp;
+                bool rowAdded = false;
+            
+                while(getline(finalFile, temp)) {
+                    string quantity = GetCellByIndex(temp, 7);
+                    string price = GetCellByIndex(temp, 8);
+                    if((stoi(quantity) >= stoi(quantityOt) && stoi(quantity) <= stoi(quantityDo)) && 
+                    ( stoi(price)  >= stoi(priceOt) && stoi(price) <= stoi(priceDo))) { 
+                        finalFinalFile << temp << endl; rowAdded = true; 
+                    }
+                }
+            
+                finalFinalFile.close();
+                finalFile.close();
+            
+                if(!rowAdded) {
+                    toClient << "!Ничего не найдено :( ))";
+                    return false;
+                }
+            
+                ifstream finalFinalFileForRead("finalFileAfterChecking_" + username + ".tmp");
+                ostringstream oss;
+                oss << finalFinalFileForRead.rdbuf(); 
+                finalFinalFileForRead.close();
+            
+                string message = oss.str(); 
+                toClient << "#";
+                toClient << message;    
+            
+                return true;
+            }
+            else{
                 toClient << "!Ничего не найдено :( ))";
                 return false;
             }
-        
-            ifstream finalFinalFileForRead("finalFileAfterChecking_" + username + ".tmp");
-            ostringstream oss;
-            oss << finalFinalFileForRead.rdbuf(); 
-            finalFinalFileForRead.close();
-        
-            string message = oss.str(); 
-            toClient << "#";
-            toClient << message;    
-        
-            return true;
-        }
-        else{
-            toClient << "!Ничего не найдено :( ))";
-            return false;
         }
     }
     else {
